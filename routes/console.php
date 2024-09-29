fce9cdf2d71f8b6378021b188259140d250fdd45
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-09-29 22:35:13 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use Nidavellir\Trading\JobPollerManager;
 
 Schedule::call(function () {
-    \Log::info('Running something ... ');
+    $jobPollerManager = new JobPollerManager;
+    $jobPollerManager->handle();
 })->everySecond();
