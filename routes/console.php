a2cb73289be56d0ed1c1921ab79c688e21aeb4fc
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-12 12:33:15 +0000

    ploi deploy

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('mjonir:sync-positions')
+    ->everyMinute();
+
+/*
+Schedule::command('mjonir:dispatch-positions')
+    ->everyMinute();
+*/
+
+Schedule::command('mjonir:refresh-data')
+    ->everyFifteenMinutes();
