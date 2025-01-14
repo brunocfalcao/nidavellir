f596f2e587016e7192b544a0a1ac04fc4f6b3b76
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-14 23:45:03 +0100

    wip

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,9 @@ Schedule::command('mjonir:dispatch-positions')
     ->everyMinute();
 */
 
+Schedule::command('mjolnir:update-recvwindow-safety-duration')
+    ->everyMinute();
+
 Schedule::command('mjolnir:update-accounts-balances')
     ->everyFifteenMinutes();
 
