7b988f2966abf91c159fb568d0c18a826ba269b5
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-02 00:40:21 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
+
+// Function to get all files in a directory
+function getFiles(dir) {
+    return fs.readdirSync(dir).map(file => path.join(dir, file));
+}
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                ...getFiles('resources/images'),
+                ...getFiles('resources/videos'),
+            ],
             refresh: true,
         }),
     ],
